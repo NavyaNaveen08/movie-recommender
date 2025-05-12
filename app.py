36ede771c051d7b3440d92daeab7b65f54c119f6
NavyaNaveen08 <166049948+NavyaNaveen08@users.noreply.github.com>
2025-05-13 00:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ tfidf_vectorizer, cosine_sim, title_to_index, movie_data = load_resources()
 # OMDb API Key (only for posters)
 # -------------------------
 
-OMDB_API_KEY = "your_omdb_api_key_here"  # <-- Replace with your OMDb API Key
+OMDB_API_KEY = "122ba293"  # <-- Replace with your OMDb API Key
 
 @st.cache_data(show_spinner=False)
 def fetch_poster(title):
